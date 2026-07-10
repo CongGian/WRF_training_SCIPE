6f78c1f1693a129d229f8c2a5cd711441d63dd15
tknight-6 <ak1817@msstate.edu>
2026-07-10 15:49:17 -0500

    Add slurm script to run pangu model

---
+++ pangu_example/run_pangu.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -l
+
+#SBATCH -A cis240917-gpu
+#SBATCH -p gpu # the default queue is "shared" queue
+#SBATCH --gpus-per-node=1
+##SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=00:01:00
+#SBATCH --mem=15G
+#SBATCH --job-name pangu
+#SBATCH --mail-user=ak1817@msstate.edu   # e-mail address
+#SBATCH --mail-type=END
+
+# Load conda and activate environment
+ml conda
+conda activate /anvil/projects/x-cis240917/WRF_training_SCIPE/pangu
+
+# Run inference.py to make a forecast
+python inference.py > log.steps
+
+# Convert the forecast output (numpy array) back to NetCDF format
+python forecast_decode.py > log.decode
